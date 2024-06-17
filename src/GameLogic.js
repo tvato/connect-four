2fa6c539802f3a8ede24937553d0a50813e532be
tvato <tvat0@protonmail.com>
2024-06-17 08:58:17 +0300

    added tie condition, fixed bug with lineWidth, fixed bug with overstacking

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -2,12 +2,16 @@ export default class GameLogic{
     constructor(){
         this.stacks = [0,0,0,0,0,0,0]
         this.isRed = true
+
+        // For tracking game state and checking winning lines
         this.gameState = [[0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0]]
+
+        // Coordinates for the draw areas
         this.coordinates = [[{x: 355, y: 600},{x: 435, y: 600},{x: 515, y: 600},{x: 595, y: 600},{x: 675, y: 600},{x: 755, y: 600},{x: 835, y: 600}],
                             [{x: 355, y: 520},{x: 435, y: 520},{x: 515, y: 520},{x: 595, y: 520},{x: 675, y: 520},{x: 755, y: 520},{x: 835, y: 520}],
                             [{x: 355, y: 440},{x: 435, y: 440},{x: 515, y: 440},{x: 595, y: 440},{x: 675, y: 440},{x: 755, y: 440},{x: 835, y: 440}],
@@ -20,7 +24,6 @@ export default class GameLogic{
         // From left to right:
         // 355, 435, 518, 595, 680, 766, 841
         // Play area range: 300 - 900
-        // 78.5 for each, +20px for leftmost and +30px rightmost
         /* Range:
             1. 320 - 390
             2. 400 - 470
@@ -31,13 +34,11 @@ export default class GameLogic{
             7. 800 - 870
         */
         var lastTurn = 0
-        console.log(this.gameState)
         switch(true){
             case x >= 320 && x <= 390 && this.stacks[0] <= 5:
                 this.isRed ? this.drawRed(355, 600 - (this.stacks[0] * 80)) : this.drawBlue(355, 600 - (this.stacks[0] * 80))
                 this.gameState[this.stacks[0]][0] = this.isRed ? 1 : 2;
                 lastTurn = this.isRed ? 1 : 2;
-                
                 this.stacks[0] = this.stacks[0] + 1
                 this.isRed = !this.isRed
                 break;
@@ -84,14 +85,29 @@ export default class GameLogic{
                 this.isRed = !this.isRed
                 break;
             default:
+                lastTurn = 0;
                 console.log("Click on the game board!");
                 break;
         }
         this.checkTurn()
         if(this.isWin(lastTurn)){
             this.clearGame();
-            //alert(lastTurn === 1 ? "Red wins!" : "Blue wins!")
         }
+
+        if(this.stacks.reduce((a,b) => a + b, 0) >= 42){
+            this.gameTied();
+        }
+        
+    }
+
+    gameTied(){
+        const ctx = document.getElementById("gameArea").getContext("2d")
+
+        ctx.clearRect(450, 0, 300, 100)
+
+        ctx.font = "30px Arial"
+        ctx.fillStyle = "black"
+        ctx.fillText("TIE!", 570, 50)
     }
 
     drawRed(x, y){
@@ -110,7 +126,7 @@ export default class GameLogic{
     drawBlue(x, y){
         const ctx = document.getElementById("gameArea").getContext("2d")
     
-        // Upper left corner (355, 200)
+        // Upper left corner coordinates (355, 200)
         // Move 80
 
         ctx.lineWidth = 1
@@ -126,6 +142,12 @@ export default class GameLogic{
     clearGame(){
         this.stacks = [0,0,0,0,0,0,0]
         this.isRed = true
+        this.gameState = [[0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0],
+                          [0,0,0,0,0,0,0]]
     }
 
     checkTurn(){
@@ -144,14 +166,11 @@ export default class GameLogic{
         console.log("start:", start)
         console.log("end:", end)
 
-        console.log("lineWidth in before win line: ", ctx.lineWidth)
-
-        ctx.moveTo(start.x, start.y /* x, y */)
-        ctx.lineTo(end.x, end.y /* x, y */)
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y)
+        ctx.lineTo(end.x, end.y)
         ctx.lineWidth = 5.0
         ctx.stroke()
-
-        console.log("lineWidth in after win line: ", ctx.lineWidth)
 
         ctx.clearRect(450, 0, 300, 100)
 
@@ -161,13 +180,12 @@ export default class GameLogic{
     }
 
     isWin(latestTurn){
-        console.log("Last turn:", latestTurn === 1 ? "red" : "blue")
+        if(latestTurn === 0) return false;
 
         // Horizontal check
         for(var i=0; i < this.gameState.length; i++){
             for(var j=0; j < this.gameState[i].length-3; j++){
                 if(this.gameState[i][j] === latestTurn && this.gameState[i][j+1] === latestTurn && this.gameState[i][j+2] === latestTurn && this.gameState[i][j+3] === latestTurn){
-                    console.log(this.coordinates[i][j])
                     this.drawWinLine(this.coordinates[i][j], this.coordinates[i][j+3], latestTurn)
                     return true
                 }
@@ -178,7 +196,6 @@ export default class GameLogic{
         for(var i=0; i < this.gameState.length-3; i++){
             for(var j=0; j < this.gameState[i].length; j++){
                 if(this.gameState[i][j] === latestTurn && this.gameState[i+1][j] === latestTurn && this.gameState[i+2][j] === latestTurn && this.gameState[i+3][j] === latestTurn){
-                    console.log(this.coordinates[i][j])
                     this.drawWinLine(this.coordinates[i][j], this.coordinates[i+3][j], latestTurn)
                     return true
                 }
@@ -189,7 +206,6 @@ export default class GameLogic{
         for(var i=0; i < this.gameState.length-3; i++){
             for(var j=3; j < this.gameState[i].length; j++){
                 if(this.gameState[i][j] === latestTurn && this.gameState[i+1][j-1] === latestTurn && this.gameState[i+2][j-2] === latestTurn && this.gameState[i+3][j-3] === latestTurn){
-                    console.log(this.coordinates[i][j])
                     this.drawWinLine(this.coordinates[i][j], this.coordinates[i+3][j-3], latestTurn)
                     return true
                 }
@@ -199,15 +215,12 @@ export default class GameLogic{
         // Diagonal check
         for(var i=3; i < this.gameState.length; i++){
             for(var j=3; j < this.gameState[i].length; j++){
-                console.log("i =", i, "j =", j)
                 if(this.gameState[i][j] === latestTurn && this.gameState[i-1][j-1] === latestTurn && this.gameState[i-2][j-2] === latestTurn && this.gameState[i-3][j-3] === latestTurn){
-                    console.log(this.coordinates[i][j])
                     this.drawWinLine(this.coordinates[i][j], this.coordinates[i-3][j-3], latestTurn)
                     return true
                 }
             }
         }
-
         return false
     }
 }
